ba1ac444e1cf75fc00c87af6db579df33f57861c
Jérôme Moret <jermoret@gmail.com>
2016-05-25 16:26:18 +0200

    Finishing models and insertions + Clickable element on table

--- PCBuild/app/models/Storage.scala
+++ PCBuild/app/models/Storage.scala
@@ -2,7 +2,16 @@ package models
 
 import play.api.db.slick.Config.driver.simple._
 
-case class Storage(id: Int, manufacturer: String, model: String, gigabytes: Int, rotationSpeed: Int, consumption: Float, popularity: Int)
+case class Storage(
+                    id: Int,
+                    manufacturer: String,
+                    model: String,
+                    gigabytes: Int,
+                    rotationSpeed: Int,
+                    imageUrl: String,
+                    price: Float,
+                    popularity: Int
+                  )
 
 class StorageTable(tag: Tag) extends Table[Storage](tag, "STORAGE") {
 
@@ -11,9 +20,10 @@ class StorageTable(tag: Tag) extends Table[Storage](tag, "STORAGE") {
   def model = column[String]("MODEL", O.NotNull)
   def gigabytes = column[Int]("GIGABYTES", O.NotNull)
   def rotationSpeed = column[Int]("ROTATION_SPEED", O.NotNull)
-  def consumption = column[Float]("ENERGY_CONSUMPTION", O.NotNull)
+  def imageUrl = column[String]("IMG_URL", O.NotNull)
+  def price = column[Float]("PRICE", O.NotNull)
 
   def popularity = column[Int]("POPULARITY", O.NotNull)
 
-  def * = (id, manufacturer, model, gigabytes, rotationSpeed, consumption, popularity) <> (Storage.tupled, Storage.unapply)
+  def * = (id, manufacturer, model, gigabytes, rotationSpeed, imageUrl, price, popularity) <> (Storage.tupled, Storage.unapply)
 }
